b517091d8c56bc04e6a8a71078e4516525878c05
chenqi <419499748@qq.com>
2022-07-26 14:41:59 +0800

    init

---
+++ Example/KKPodTest726/KK726ViewController.h
@@ -0,0 +1,13 @@
+//
+//  KK726ViewController.h
+//  KKPodTest726
+//
+//  Created by chenqi on 07/26/2022.
+//  Copyright (c) 2022 chenqi. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KK726ViewController : UIViewController
+
+@end
---
+++ Example/KKPodTest726/KK726ViewController.m
@@ -0,0 +1,29 @@
+//
+//  KK726ViewController.m
+//  KKPodTest726
+//
+//  Created by chenqi on 07/26/2022.
+//  Copyright (c) 2022 chenqi. All rights reserved.
+//
+
+#import "KK726ViewController.h"
+
+@interface KK726ViewController ()
+
+@end
+
+@implementation KK726ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
